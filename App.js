47dacfaddb8c17ba8602a3bfb9b31fee48e49e89
yehsangoh <jaws0910@gmail.com>
2020-06-29 00:48:52 +0900

    openweather API를 이용해서 날씨 정보 가져오기

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import {Alert} from "react-native"
+import axios from "axios";
 
-export default function App() {
-  return (
-    <Loading />
-  );
+const API_KEY = "4b20fe0c8079b33e15a8dacc276841b9";
+
+export default class extends React.Component {
+    state = {
+        isLoading: true,
+        latitude: 0, 
+        longitude: 0
+    };
+
+    getWeather = async(latitude, longitude) => {
+        const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+        console.log(data);
+    };
+
+    getLocation = async() => {
+        try {
+            const respoawait = await Location.requestPermissionsAsync();
+            console.log(respoawait);
+
+            const {
+                coords: {latitude, longitude}
+            } = await Location.getCurrentPositionAsync();
+            
+            this.getWeather(latitude, longitude);
+
+            this.setState({ 
+                isLoading: false,
+                latitude: latitude, 
+                longitude: longitude
+            });
+
+        } catch (error) {
+            Alert.alert("Can't find you", "So sad");
+        }
+    };
+
+    componentDidMount() {
+        this.getLocation();
+    };
+
+    render() {
+        const { isLoading } = this.state;
+        return isLoading ? <Loading /> : null; 
+    };
 }
 
