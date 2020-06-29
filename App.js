a32be858b132f1a66cbd404d75e0f80e0885d31d
yehsangoh <jaws0910@gmail.com>
2020-06-29 23:06:38 +0900

    no message

--- App.js
+++ App.js
@@ -12,13 +12,19 @@ export default class extends React.Component {
     state = {
         isLoading: true,
         latitude: 0, 
-        longitude: 0
+        longitude: 0,
+        condition: "Clear",
     };
 
     getWeather = async(latitude, longitude) => {
-        const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+        const { 
+            data: {
+                main: {temp},
+                weather
+            } 
+        } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
         
-        this.setState({isLoading: false, temp: data.main.temp})
+        this.setState({isLoading: false, temp: temp, condition=weather[0].main })
     };
 
     getLocation = async() => {
@@ -34,7 +40,7 @@ export default class extends React.Component {
 
             this.setState({ 
                 isLoading: false,
-                latitude: latitude, 
+                latitude: latitude,  
                 longitude: longitude
             });
 
@@ -48,8 +54,8 @@ export default class extends React.Component {
     };
 
     render() {
-        const { isLoading, temp } = this.state;
-        return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />; 
+        const { isLoading, temp, condition } = this.state;
+        return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />; 
     };
 }
 
